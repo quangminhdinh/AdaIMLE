b3a6b4a4c82bb3ea08d162bf60b3daa0357d9b88
Quang Minh Dinh <qmd@sfu.ca>
2025-05-16 10:41:41 +0700

    add: missing packages

--- run-train.sh
+++ run-train.sh
@@ -56,12 +56,12 @@ exec torchrun --nproc_per_node=$(echo $CUDA_VISIBLE_DEVICES | awk -F',' '{print 
     --imle_force_resample 5  \
     --lr 0.0002 \
     --iters_per_ckpt 100000 --iters_per_images 5000 --iters_per_save 1000 \
-    --search_type ‘lpips’ \
+    --search_type 'lpips' \
     --n_batch 8 \
     --num_epochs 500 \
     --fid_freq 10 \
     --imle_batch 32 \
     --compile True \
     --use_multi_res True \
-    --multi_res_scales ‘32,64,128’ \
-    --dec_blocks ‘1x2,4m1,4x3,8m4,8x4,16m8,16x9,32m16,32x21,64m32,64x13,128m64,128x7,256m128’ \
+    --multi_res_scales '32,64,128' \
+    --dec_blocks '1x2,4m1,4x3,8m4,8x4,16m8,16x9,32m16,32x21,64m32,64x13,128m64,128x7,256m128'
---
+++ train-uv.sh
@@ -0,0 +1,18 @@
+torchrun --nproc_per_node=1 --standalone train.py --hps fewshot \
+    --save_dir ./results/flowers_i \
+    --data_root ./datasets/flowers_i/ \
+    --dataset flowers102-i \
+    --wandb_name new_imle \
+    --force_factor 5 \
+    --imle_force_resample 5  \
+    --lr 0.0002 \
+    --iters_per_ckpt 100000 --iters_per_images 5000 --iters_per_save 1000 \
+    --search_type 'lpips' \
+    --n_batch 1 \
+    --num_epochs 500 \
+    --fid_freq 10 \
+    --imle_batch 32 \
+    --compile True \
+    --use_multi_res True \
+    --multi_res_scales '32,64,128' \
+    --dec_blocks '1x2,4m1,4x3,8m4,8x4,16m8,16x9,32m16,32x21,64m32,64x13,128m64,128x7,256m128'
